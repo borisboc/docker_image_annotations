9bfb8631be2f642011eeb8ededc9a0d2e6c94df9
Boris Bocquet <borisboc@free.fr>
2025-02-13 19:45:37 +0100

    Now you can also have GPU for fiftyone

--- start_gpu.sh
+++ start_gpu.sh
@@ -1,2 +1,4 @@
 docker container rm img-annotations-sa2-label-studio ;\
-docker compose -f compose_local_files.yaml --profile gpu -p image_annotations up
+docker container rm img-annotations-fiftyone ;\
+echo "starting with profile gpu" && \
+IMG_ANNOTATIONS_BUILD_PROFILE=gpu docker compose -f compose_local_files.yaml --profile gpu -p image_annotations up --build
