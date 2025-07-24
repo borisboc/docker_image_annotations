3872e542a0b6d08bf786b6636533a0bd6f66d283
Boris Bocquet <b.bocquet@akeoplus.com>
2025-07-24 17:34:03 +0200

    do not force rebuild in the compose

--- start_cpu.sh
+++ start_cpu.sh
@@ -1,4 +1,4 @@
 docker container rm img-annotations-sa2-label-studio ;\
 docker container rm img-annotations-fiftyone ;\
 echo "starting with profile cpu" && \
-IMG_ANNOTATIONS_BUILD_PROFILE=cpu docker compose -f compose_local_files.yaml --profile cpu -p image_annotations up --build
+IMG_ANNOTATIONS_BUILD_PROFILE=cpu docker compose -f compose_local_files.yaml --profile cpu -p image_annotations up
