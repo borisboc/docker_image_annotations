208267b210c0ed3a7ebbdccc6a3cab6737b763d8
Boris Bocquet <borisboc@free.fr>
2025-08-19 05:00:35 +0200

    You can now pass extra args to start_cpu.sh and start_gpu.sh

--- start_gpu.sh
+++ start_gpu.sh
@@ -1,4 +1,4 @@
 docker container rm img-annotations-sa2-label-studio ;\
 docker container rm img-annotations-fiftyone ;\
 echo "starting with profile gpu" && \
-IMG_ANNOTATIONS_BUILD_PROFILE=gpu docker compose -f compose_local_files.yaml --profile gpu -p image_annotations up
+IMG_ANNOTATIONS_BUILD_PROFILE=gpu docker compose -f compose_local_files.yaml --profile gpu -p image_annotations up $1
