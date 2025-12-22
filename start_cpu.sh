a0de28f18615b290752e85dbc9729b201d7d5ae4
Boris Bocquet <borisboc@free.fr>
2025-12-22 10:36:21 +0100

    you can pass multiple args to the 2 shell scripts

--- start_cpu.sh
+++ start_cpu.sh
@@ -1,4 +1,4 @@
 docker container rm img-ann-sa2-label-studio ;\
 docker container rm img-ann-fiftyone ;\
 echo "starting with profile cpu" && \
-IMG_ANNOTATIONS_BUILD_PROFILE=cpu docker compose -f compose_local_files.yaml --profile cpu -p img-ann up $1
+IMG_ANNOTATIONS_BUILD_PROFILE=cpu docker compose -f compose_local_files.yaml --profile cpu -p img-ann up $@
